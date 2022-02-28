3a59f6815cb11563a5ebf1eff51a238444cc340a
Amy Shi <amyhshi92@gmail.com>
2022-02-28 14:58:29 -0500

    commit tree 58933809938dc67afa0b365af6ddf5908c37f364

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(response => response.json())
+  .then(data => {
+    for (const url of data.message){
+      const image = document.createElement("img")
+      image.src = url
+      document.querySelector("#dog-image-container").appendChild(image)
+    }
+  })
+
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(data => {
+    const breeds = Object.keys(data.message)
+    for (const breed of breeds) {
+      const item = document.createElement("li")
+      item.textContent = breed
+      item.style.cursor = 'pointer'
+      item.addEventListener("click", () => item.style.color = "red")
+      document.querySelector("#dog-breeds").appendChild(item)
+    }
+
+    const select = document.querySelector("select")
+    select.addEventListener("change", () => {
+      const letter = select.options[select.selectedIndex].value
+      const dog = document.querySelectorAll("li")
+      dog.forEach(element => {
+        if (element.textContent.startsWith(letter)) {
+          element.style.display = ""
+        }
+        else {element.style.display = "none"}
+      })
+    })
+  })
+})
